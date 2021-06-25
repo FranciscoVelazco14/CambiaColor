95e74f75ebc352e79adcb94645fa01f254b2b552
FranciscoVelazco14 <Franciscovelazco@live.com>
2021-06-25 11:36:45 -0500

    Primer commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function cambiarColor(x){
+switch(x){
+    case 1:
+        document.querySelector("body").style.backgroundColor = "red";
+        break;
+    case 2:
+        document.querySelector("body").style.backgroundColor = "green";
+        break;
+    case 3:
+        document.querySelector("body").style.backgroundColor = "blue";
+        break;
+    case 4:
+        document.querySelector("body").style.backgroundColor = "powderblue";
+        break;    
+}
+}
+
